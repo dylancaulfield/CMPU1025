82ea79cfe5a446c10b07d60232213660b21e9c55
Dylan Caulfield <dpcdylan@gmail.com>
2020-02-21 20:04:02 +0000

    Added frequency

--- c/assignments/2/assignment2.c
+++ c/assignments/2/assignment2.c
@@ -13,24 +13,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LENGTH 6
+#define LOTTO_LENGTH 6
+#define FREQ_LENGTH 42
 
 // Function Prototypes
 void bubbleSort(int *, int);
-int arrayContains(int *, int, int);
-void chooseLottoNumbers(int *);
-void compareChosenNumbers(int *);
-int getMenuSelection(void);
 void printArray(int *, int);
 void showChosenNumbers(int *);
 void sortNumbers(int *);
+void chooseLottoNumbers(int *);
+void compareChosenNumbers(int *);
+void showLottoFrequency(int *);
+int arrayContains(int *, int, int);
+int getMenuSelection(void);
+int *getFrequency(int *);
+
 
 int main()
 {
 
     int unlocked = 0;
     int menuSelection;
-    int chosenNumbers[LENGTH] = {};
+    int chosenNumbers[LOTTO_LENGTH] = {};
 
     do
     {
@@ -48,36 +52,36 @@ int main()
 
         switch (menuSelection)
         {
-        case 1: // Choose numbers
-        {
+            case 1: // Choose numbers
+            {
 
-            chooseLottoNumbers(chosenNumbers);
-            break;
-        }
-        case 2: // Show chosen numbers
-        {
+                chooseLottoNumbers(chosenNumbers);
+                break;
+            }
+            case 2: // Show chosen numbers
+            {
 
-            showChosenNumbers(chosenNumbers);
-            break;
-        }
-        case 3: // Sort chosen numbers
-        {
+                showChosenNumbers(chosenNumbers);
+                break;
+            }
+            case 3: // Sort chosen numbers
+            {
 
-            sortNumbers(chosenNumbers);
-            break;
-        }
-        case 4: // Compare with winning numbers
-        {
+                sortNumbers(chosenNumbers);
+                break;
+            }
+            case 4: // Compare with winning numbers
+            {
 
-            compareChosenNumbers(chosenNumbers);
-            break;
-        }
-        case 5: // Display frequency
-        {
+                compareChosenNumbers(chosenNumbers);
+                break;
+            }
+            case 5: // Display frequency
+            {
 
-            /* TODO */
-            break;
-        }
+                showLottoFrequency(chosenNumbers);
+                break;
+            }
         } // End switch
 
     } while (menuSelection != 6);
@@ -137,7 +141,14 @@ void chooseLottoNumbers(int *array)
 
     int i, enteredNum, numInRange, numUsed;
 
-    for (i = 0; i < LENGTH; i++)
+    // Reset each element to 0
+    for (i = 0; i < LOTTO_LENGTH; i++)
+    {
+
+        *(array + i) = 0;
+    }
+
+    for (i = 0; i < LOTTO_LENGTH; i++)
     {
 
         do
@@ -145,11 +156,10 @@ void chooseLottoNumbers(int *array)
 
             printf("\nEnter #%d: ", i + 1);
             scanf(" %d", &enteredNum);
-            while (getchar() != '\n')
-                ; // Clear standard input buffer
+            while (getchar() != '\n') {} // Clear standard input buffer
 
             numInRange = enteredNum >= 1 && enteredNum <= 42;
-            numUsed = arrayContains(array, LENGTH, enteredNum);
+            numUsed = arrayContains(array, LOTTO_LENGTH, enteredNum);
 
             if (!numInRange)
             {
@@ -168,6 +178,9 @@ void chooseLottoNumbers(int *array)
         *(array + i) = enteredNum;
     }
 
+    // Update frequency of the chosen numbers
+    getFrequency(array);
+
     printf("\n");
 }
 
@@ -177,12 +190,12 @@ void compareChosenNumbers(int *array)
 {
 
     int i, matched = 0;
-    int winningNumbers[LENGTH] = {1, 3, 5, 7, 9, 11};
+    int winningNumbers[LOTTO_LENGTH] = {1, 3, 5, 7, 9, 11};
 
-    for (i = 0; i < LENGTH; i++)
+    for (i = 0; i < LOTTO_LENGTH; i++)
     {
 
-        if (arrayContains(array, LENGTH, *(winningNumbers + i)))
+        if (arrayContains(array, LOTTO_LENGTH, *(winningNumbers + i)))
         {
             matched++;
         }
@@ -193,40 +206,40 @@ void compareChosenNumbers(int *array)
 
     switch (matched)
     {
-    case 3:
-    {
+        case 3:
+        {
 
-        printf("Congratulations! You have won a Cinema Pass\n\n");
-        break;
-    }
+            printf("Congratulations! You have won a Cinema Pass\n\n");
+            break;
+        }
 
-    case 4:
-    {
+        case 4:
+        {
 
-        printf("Congratulations! You have won a Weekend Away\n\n");
-        break;
-    }
+            printf("Congratulations! You have won a Weekend Away\n\n");
+            break;
+        }
 
-    case 5:
-    {
+        case 5:
+        {
 
-        printf("Congratulations! You have won a New Car\n\n");
-        break;
-    }
+            printf("Congratulations! You have won a New Car\n\n");
+            break;
+        }
 
-    case 6:
-    {
+        case 6:
+        {
 
-        printf("Congratulations! You have won the Jackpot\n\n");
-        break;
-    }
+            printf("Congratulations! You have won the Jackpot\n\n");
+            break;
+        }
 
-    default:
-    {
+        default:
+        {
 
-        printf("Better luck next time!\n\n");
-        break;
-    }
+            printf("Better luck next time!\n\n");
+            break;
+        }
     } // End Switch
 }
 
@@ -249,8 +262,7 @@ int getMenuSelection()
         printf("Enter Selection: ");
 
         scanf(" %d", &selection);
-        while (getchar() != '\n')
-            ; // Clear standard input buffer
+        while (getchar() != '\n') {} // Clear standard input buffer
 
         inRange = selection >= 1 && selection <= 6;
 
@@ -294,22 +306,61 @@ void showChosenNumbers(int *array)
 {
 
     printf("The numbers you have chosen are:\n\n");
-    printArray(array, LENGTH);
+    printArray(array, LOTTO_LENGTH);
     printf("\n\n");
 }
 
+// Function to show to user the Lotto numbers are being sorted
+// and to call bubble sort
 void sortNumbers(int *array)
 {
 
     printf("Sorting chosen Lotto numbers:\n\n");
-    printArray(array, LENGTH);
+    printArray(array, LOTTO_LENGTH);
     printf(" -> ");
 
     // Sort the array
-    bubbleSort(array, LENGTH);
+    bubbleSort(array, LOTTO_LENGTH);
 
-    printArray(array, LENGTH);
+    printArray(array, LOTTO_LENGTH);
 
     printf("\n\n");
+}
 
+int *getFrequency(int *update)
+{
+
+    static int frequency[FREQ_LENGTH] = {};
+    int i;
+
+    if (update != NULL)
+    {
+
+        for (i = 0; i < LOTTO_LENGTH; i++)
+        {
+
+            // Explain this properly!!
+            (*(frequency + *(update + i) - 1))++;
+        }
+    }
+
+    return frequency;
+}
+
+// Function to show how many times the current Lotto numbers have been chosen
+void showLottoFrequency(int *array)
+{
+
+    int i, f;
+    int *frequency = getFrequency(NULL);
+
+    for (i = 0; i < LOTTO_LENGTH; i++)
+    {
+
+        f = *(frequency + *(array + i) - 1);
+
+        printf("%d has appeared %d time%c\n", *(array + i), f, f == 1 ? ' ' : 's');
+    }
+
+    printf("\n\n");
 }
